f23c49236325d70c9f2774010ca0ee0684a6cd5a
leopoldfrey <leopold.frey@free.fr>
2018-11-23 10:10:32 +0100

    correction remotePoint

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1563,7 +1563,7 @@ void ofApp::playLine() {
     }
     if(BR != leapZero() && BR != pBR)
     {
-        addRemotePoint(true, BR);
+        addRemotePoint(false, BR);
         pBR = BR;
     }
 }
